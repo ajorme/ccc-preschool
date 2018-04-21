b38f1ac085df641322be47d89eafed0bfda721ff
ajorme <orme_alexander_j@lilly.com>
2018-04-21 18:55:24 -0400

    Updated for usage with react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter,Route, Switch} from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" component={App} />
+        </Switch>
+      </div>
+    </BrowserRouter>,
+  document.getElementById('root')
+);
 registerServiceWorker();
